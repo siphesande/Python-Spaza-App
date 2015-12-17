a314999f8b9315f3ccd5bbf2168020c2b07ee956
siphesande <siphesande@projectcodex.co>
2015-12-17 17:21:15 +0200

    python

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
-from flask import Flask, render_template, flash, request, url_for, redirect
+from flask import Flask, render_template, flash, request, url_for, redirect,session
 from wtforms import Form
+from wtforms import Form, BooleanField, TextField, PasswordField, validators
+from dbconnect import connection
+from passlib.hash import sha256_crypt
+from MySQLdb import escape_string as thwart
+import gc
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -27,22 +34,98 @@ def logout():
 	flash('You have been logged out.')
 	gc.collect()
 	return redirect(url_for('main'))
-class RegistratiopnForm(Form):
-	username = TextField("Username", [validators.Length(min =4, max =20)])
-	email = TextField("Email Address", [validators.Length(min = 6, max= 50)])
-	password = PasswordField("Password", [validators.Required(),
-		                                  validators.EqualTo("confirm", message = "Passwords Must macth." )])
-	confirm = PasswordField("Repeat Password")
-	accept_tos = BooleanField("I accept the <a href="/tos/">Terms of Service and the <a href = "/privacy/">Privacy Notice </a> (Last updated Dec 2015)",[validators.Required])
 
 
+class RegistrationForm(Form):
+    username = TextField('Username', [validators.Length(min=4, max=20)])
+    email = TextField('Email Address', [validators.Length(min=6, max=50)])
+    password = PasswordField('New Password', [validators.Required(),
+                                              validators.EqualTo('confirm', message='Passwords must match')])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the <a href="/about/tos" target="blank">Terms of Service</a> and <a href="/about/privacy-policy" target="blank">Privacy Notice</a> (updated Jan 22, 2015)', [validators.Required()])
+
+# class RegistratiopnForm(Form):
+# 	username = TextField("Username", [validators.Length(min =4, max =20)])
+# 	email = TextField("Email Address", [validators.Length(min = 6, max= 50)])
+# 	password = PasswordField("Password", [validators.Required(),
+# 		                                  validators.EqualTo("confirm", message = "Passwords Must macth." )])
+# 	confirm = PasswordField("Repeat Password")
+# 	accept_tos = BooleanField("I accept the <a href='/about/tos/'>Terms of Service and the <a href = '/privacy/'>Privacy Notice </a> (Last updated Dec 2015)",[validators.Required])
 @app.route('/register/', methods=['GET', 'POST'])
 def register_page():
-	try:
-		c, conn = connection()
-		return("okay")
-	except Exception as e:
-		return(str(e))
+
+    try:
+        form = RegistrationForm(request.form)
+        if request.method == 'POST' and form.validate():
+            #flash("register attempted")
+
+            username = form.username.data
+            email = form.email.data
+
+            password = sha256_crypt.encrypt((str(form.password.data)))
+            c,conn = connection()
+
+            x = c.execute("SELECT * FROM users WHERE username = (%s)",
+                (thwart(username)))
+
+            if int(x) > 0:
+                flash("That username is already taken, please choose another")
+                return render_template('register.html', form=form)
+
+            else:
+                c.execute("INSERT INTO users (username, password, email, tracking) VALUES (%s, %s, %s, %s)",
+                    (thwart(username), thwart(password), thwart(email), thwart("/introduction-to-python-programming/")))
+                conn.commit()
+                flash('Thanks for registering')
+                c.close()
+                conn.close()
+                gc.collect()
+                session['logged_in'] = True
+                session['username'] = username
+                return redirect(url_for('intro_to_py'))
+        gc.collect()
+        #flash("hi there.")
+        return render_template('register.html', form=form)
+    except Exception as e:
+        return(str(e))
+
+
+# @app.route('/register/', methods=['GET', 'POST'])
+# def register_page():
+# 	try:
+# 		form = RegistrationForm(request.form)
+# 		if request.method == "POST" and form.validate():
+# 			username = form.username.date
+# 			email =form.email.data
+# 			password = sha256_crypt.encrypt(((str(form.password.date))))
+#             c, conn = connection() 
+
+#             x = c.execute("SELECT * FROM users WHERE username = (%s)",
+#             	(thwart(username)))
+#             if int(x) > 0:
+#             	flash("That username is already taken, please choose another")
+#             	return render_template('register.html', form-form)
+
+#             else:
+#             	c.execute("INSERT INTO users (username,password,email, tracking) VALUES (%s, %s, %s, %s),
+#                        (thwart(username),thwart(password),thwart(email),thwart("/introduction-to-python-programming/"))
+#                 conn.commit()
+#                 flash("Thanks for registering!")
+#                 c.close()
+#                 conn.close()
+
+#                 gc.collect()
+
+#                 session['logged_in'] = True
+#                 session['username'] = username
+
+#                 return redirect(url_for("dashboard"))
+
+# return render_template("register.html", form=form)
+
+
+# 	except Exception as e:
+# 		return(str(e))
 
     
 
